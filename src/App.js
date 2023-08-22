0805da8317fe1d131eed875eb555bdb590105b12
mua-wallace <muawallizewi@gmail.com>
2023-08-22 16:08:56 +0100

    use of useParams hook

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Survey from "./components/Survey";
+import UseParams from "./components/UseParams";
+import ParamsComponent from "./components/ParamsComponent";
 
 function App() {
   return (
     <BrowserRouter>
       <h1>Routing using react router dom</h1>
       <Routes>
+        <Route path="/useparams" element={<UseParams />} />
+        <Route path="/:word" element={<ParamsComponent />} />
+        <Route path="/:word/:color/:bgColor" element={<ParamsComponent />} />
         <Route path="/about" element={<About />} />
         <Route exact path="/" element={<Home />} />
         <Route exact path="/survey" element={<Survey />} />
---
+++ src/components/UseParams.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UseParams = (props) => {
+  return (
+    <div>Welcome!!!</div>
+  )
+}
+
+export default UseParams
